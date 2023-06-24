82ee1ea4c444e63b7e7f2141f695e517e6972b2e
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-06-24 17:24:11 +0100

    Set up project with initial files
    - Creates project folder
    - Sets up Tailwind CSS
    - Sets up React
    - Sets up TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				black: {
+					1: "#323544",
+					2: "#0000001a",
+					3: "#262936",
+				},
+				white: {
+					DEFAULT: "#fff",
+					1: "#bfc1c8",
+				},
+				blue: {
+					"1": "#009ad8",
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
